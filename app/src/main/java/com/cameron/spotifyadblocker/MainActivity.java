f600b1878dfd550bf39f557c419ca0a19d3b0432
leapwill <leapwill@github.com>
2017-06-17 18:58:23 -0500

    added ability to add more filters, updated built-in list

--- app/src/main/java/com/cameron/spotifyadblocker/MainActivity.java
+++ app/src/main/java/com/cameron/spotifyadblocker/MainActivity.java
@@ -1,11 +1,14 @@
 package com.cameron.spotifyadblocker;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     private boolean enabled;
@@ -35,5 +38,13 @@ public class MainActivity extends AppCompatActivity {
 
     public void notificationAccess(View view) {
         startActivity(new Intent("android.settings.ACTION_NOTIFICATION_LISTENER_SETTINGS"));
+    }
+
+    public void addAdditionalFilter(View view) {
+        SharedPreferences.Editor preferencesEditor = getSharedPreferences("additionalFilters", MODE_PRIVATE).edit();
+        EditText et = (EditText)view.getRootView().findViewById(R.id.editTextAddFilter);
+        String newFilter = et.getText().toString();
+        preferencesEditor.putString("filter_" + newFilter, newFilter);
+        preferencesEditor.apply();
     }
 }
